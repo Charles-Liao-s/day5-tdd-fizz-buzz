eeac4e53efb195cc7210c1beb6b99ce272d85380
Charles Liao <charles.liao@oocl.com>
2025-09-05 15:47:03 +0800

    feat:add function of command L

--- src/main/java/tdd/fizzbuzz/MarsRover.java
+++ src/main/java/tdd/fizzbuzz/MarsRover.java
@@ -21,21 +21,22 @@ public class MarsRover {
                     break;
             }
         }
-//        else if(command.equals("L")){
-//            switch (direction) {
-//                case "N":
-//                    direction1 = new Direction(x, y, "W");
-//                    break;
-//                case "S":
-//                    direction1 = new Direction(x, y, "E");
-//                    break;
-//                case "E":
-//                    direction1 = new Direction(x, y, "N");
-//                    break;
-//                case "W":
-//                    direction1 = new Direction(x, y, "S");
-//                    break;
-//            }
+        else if(command.equals("L")) {
+            switch (direction) {
+                case "N":
+                    direction1 = new Direction(x, y, "W");
+                    break;
+                case "S":
+                    direction1 = new Direction(x, y, "E");
+                    break;
+                case "E":
+                    direction1 = new Direction(x, y, "N");
+                    break;
+                case "W":
+                    direction1 = new Direction(x, y, "S");
+                    break;
+            }
+        }
         return direction1;
     }
 }
--- src/test/java/tdd/fizzbuzz/MarsRoverTest.java
+++ src/test/java/tdd/fizzbuzz/MarsRoverTest.java
@@ -67,67 +67,67 @@ public class MarsRoverTest {
 
     }
 
-//    @Test
-//    public void should_return_0_0_N_when_input_0_0_N_and_command_L() {
-//        int x = 0;
-//        int y = 0;
-//        String direction = "N";
-//        String command = "M";
-//
-//        MarsRover marsRover = new MarsRover();
-//
-//        Direction dir = marsRover.arc(x, y, direction, command);
-//        assert dir.getX() == 0;
-//        assert dir.getY() == 0;
-//        assert dir.getDirection().equals("W");
-//
-//    }
-//
-//    @Test
-//    public void should_return_0_0_E_when_input_0_0_E_and_command_L() {
-//        int x = 0;
-//        int y = 0;
-//        String direction = "E";
-//        String command = "M";
-//
-//        MarsRover marsRover = new MarsRover();
-//
-//        Direction dir = marsRover.arc(x, y, direction, command);
-//        assert dir.getX() == 1;
-//        assert dir.getY() == 0;
-//        assert dir.getDirection().equals("E");
-//
-//    }
-//
-//    @Test
-//    public void should_return_0_0_S_when_input_0_0_S_and_command_L() {
-//        int x = 0;
-//        int y = 0;
-//        String direction = "S";
-//        String command = "M";
-//
-//        MarsRover marsRover = new MarsRover();
-//
-//        Direction dir = marsRover.arc(x, y, direction, command);
-//        assert dir.getX() == 0;
-//        assert dir.getY() == -1;
-//        assert dir.getDirection().equals("S");
-//
-//    }
-//
-//    @Test
-//    public void should_return_0_0_W_when_input_0_0_W_and_command_L() {
-//        int x = 0;
-//        int y = 0;
-//        String direction = "W";
-//        String command = "M";
-//
-//        MarsRover marsRover = new MarsRover();
-//
-//        Direction dir = marsRover.arc(x, y, direction, command);
-//        assert dir.getX() == -1;
-//        assert dir.getY() == 0;
-//        assert dir.getDirection().equals("W");
-//
-//    }
+    @Test
+    public void should_return_0_0_W_when_input_0_0_N_and_command_L() {
+        int x = 0;
+        int y = 0;
+        String direction = "N";
+        String command = "L";
+
+        MarsRover marsRover = new MarsRover();
+
+        Direction dir = marsRover.arc(x, y, direction, command);
+        assert dir.getX() == 0;
+        assert dir.getY() == 0;
+        assert dir.getDirection().equals("W");
+
+    }
+
+    @Test
+    public void should_return_0_0_N_when_input_0_0_E_and_command_L() {
+        int x = 0;
+        int y = 0;
+        String direction = "E";
+        String command = "L";
+
+        MarsRover marsRover = new MarsRover();
+
+        Direction dir = marsRover.arc(x, y, direction, command);
+        assert dir.getX() == 0;
+        assert dir.getY() == 0;
+        assert dir.getDirection().equals("N");
+
+    }
+
+    @Test
+    public void should_return_0_0_E_when_input_0_0_S_and_command_L() {
+        int x = 0;
+        int y = 0;
+        String direction = "S";
+        String command = "L";
+
+        MarsRover marsRover = new MarsRover();
+
+        Direction dir = marsRover.arc(x, y, direction, command);
+        assert dir.getX() == 0;
+        assert dir.getY() == 0;
+        assert dir.getDirection().equals("E");
+
+    }
+
+    @Test
+    public void should_return_0_0_N_when_input_0_0_W_and_command_L() {
+        int x = 0;
+        int y = 0;
+        String direction = "W";
+        String command = "L";
+
+        MarsRover marsRover = new MarsRover();
+
+        Direction dir = marsRover.arc(x, y, direction, command);
+        assert dir.getX() == 0;
+        assert dir.getY() == 0;
+        assert dir.getDirection().equals("S");
+
+    }
 }
