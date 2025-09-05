954e5b617cf14028672da0cc92714ef1d6b675a9
Charles Liao <charles.liao@oocl.com>
2025-09-05 11:19:16 +0800

    feat:add should_return_2_when_input_2

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,9 @@
 package tdd.fizzbuzz;
 
+import java.util.*;
+
 public class FizzBuzz {
+    public String countOff(int order) {
+        return "" + order;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,52 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_input_1() {
+        int order = 1;
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String word = fizzBuzz.countOff(order);
+
+        assert word.equals("1");
+
+    }
+
+    @Test
+    public void should_return_2_when_input_2() {
+        int order = 2;
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String word = fizzBuzz.countOff(order);
+
+        assert word.equals("2");
+
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_3() {
+        int order = 3;
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String word = fizzBuzz.countOff(order);
+
+        assert word.equals("Fizz");
+
+    }
+//    @Test
+//    public void should_return_1_when_input_1(){
+//        int order =1;
+//
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//
+//        String word = fizzBuzz.countOff(order);
+//
+//        assert word.equals("1");
+//
+//    }
 }
