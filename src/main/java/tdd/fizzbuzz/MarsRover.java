88746fc60e6e10dda40e252a2241ed569ba969c4
Charles Liao <charles.liao@oocl.com>
2025-09-07 09:47:56 +0800

    feat: add BEHIND command and corresponding movement logic

--- src/main/java/tdd/fizzbuzz/MarsRover.java
+++ src/main/java/tdd/fizzbuzz/MarsRover.java
@@ -9,6 +9,7 @@ public class MarsRover {
     public static final String MOVE = "M";
     public static final String LEFT = "L";
     public static final String RIGHT = "R";
+    public static final String BEHIND = "B";
 
     public Location arc(int x, int y, String direction, String command) {
         Location latestLocation = new Location(x, y, direction);
@@ -26,6 +27,28 @@ public class MarsRover {
             case RIGHT:
                 latestLocation = commandR(x, y, direction, latestLocation);
                 break;
+            case BEHIND:
+                latestLocation = commandB(x, y, direction, latestLocation);
+                break;
+        }
+        return latestLocation;
+    }
+
+    private static Location commandB(int x, int y, String direction, Location latestLocation) {
+        switch (direction) {
+            case North:
+                latestLocation = new Location(x, y-1, South);
+                break;
+            case South:
+                latestLocation = new Location(x, y+1, North);
+                break;
+            case East:
+                latestLocation = new Location(x-1, y, W);
+                break;
+            case W:
+                latestLocation = new Location(x+1, y, East);
+                break;
+
         }
         return latestLocation;
     }
